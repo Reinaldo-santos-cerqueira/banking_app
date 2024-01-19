a6d8ebd48c231dda192b05273390090faa04d1b0
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-19 14:10:36 -0300

    feat: implement SplashSreen.hide()

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import { Text } from '@components';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from '@theme';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.ReactNode {
+    useEffect(() => {
+        SplashScreen.hide();
+    }, []);
     return (
         <SafeAreaProvider>
             <ThemeProvider theme={theme}>

c51822751c5f01c33d7cf84689122bd07f681717
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-18 22:40:26 -0300

    feat: added news pathings

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,18 @@
 module.exports = {
     presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        ['module-resolver', {
+            root: '.',
+            alias: {
+                '@components': './src/components',
+                '@screen': './src/screen',
+                '@utils': './src/utils',
+                '@routes': './src/routes',
+                '@models': './src/models',
+                '@api': './src/api',
+                '@service': './src/service',
+                '@theme': './src/theme'
+            }
+        }]
+    ]
 };

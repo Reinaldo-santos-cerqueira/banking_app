e7e2c725dbc0a3491d5ca0b1d88b837982affd25
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-19 14:08:42 -0300

    chore: configure splash screen project

--- android/app/src/main/java/com/banking/MainActivity.kt
+++ android/app/src/main/java/com/banking/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.banking
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
+import org.devio.rn.splashscreen.SplashScreen
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
-
+  override fun onCreate(savedInstanceState: Bundle?) {
+    SplashScreen.show(this)
+    super.onCreate(savedInstanceState)
+  }
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -20,3 +25,4 @@ class MainActivity : ReactActivity() {
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
+  

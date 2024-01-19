0d0d5d5f2af80d84fb9545b8c972552c1c48d771
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-19 10:12:58 -0300

    feat: testing use to text components

--- App.tsx
+++ App.tsx
@@ -1,8 +1,16 @@
+import { Text } from '@components';
+import { ThemeProvider } from '@shopify/restyle';
+import { theme } from '@theme';
 import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 function App(): React.ReactNode {
     return (
-        <></>
+        <SafeAreaProvider>
+            <ThemeProvider theme={theme}>
+                <Text mt='s20' variant='subTitleSlider'>Teste</Text>
+            </ThemeProvider>
+        </SafeAreaProvider>
     );
 }
 

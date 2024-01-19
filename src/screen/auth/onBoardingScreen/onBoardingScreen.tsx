09760076240d3f311bd69473b67c4071b7350bf9
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-19 17:33:13 -0300

    feat: starting onBoardingScreen and export item

---
+++ src/screen/auth/onBoardingScreen/onBoardingScreen.tsx
@@ -0,0 +1,11 @@
+import { Screen } from '@components';
+import React from 'react';
+
+export function OnBoardingScreen(): React.ReactNode {
+    return (
+
+        <Screen canGoBack>
+            <></>
+        </Screen>
+    );
+}

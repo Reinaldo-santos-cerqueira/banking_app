b232f82da1c783ac276c1b6cce7999407c1935d8
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-22 19:41:52 -0300

    feat: added function goLogin

--- src/components/flatOnboarding/flatOnboarding.tsx
+++ src/components/flatOnboarding/flatOnboarding.tsx
@@ -4,14 +4,29 @@ import { FlatList, ViewToken } from 'react-native';
 import { ArrayItems } from './arrayItem';
 import { ItemFlat } from './itemFlat/item';
 
-export function FlatOnboarding() {
+interface props {
+    goLogin: () => void;
+}
+
+export function FlatOnboarding({ goLogin }: props) {
+
     const [positionSlider, setPositionSlider] = useState(0);
     const onViewableItemsChanged = useCallback(({ viewableItems }: { viewableItems: ViewToken[] }) => {
         setPositionSlider(Number(viewableItems[0].index));
     }, []);
     const flatListRef = useRef<FlatList>(null);
+    function canGoLogin() {
+        goLogin();
+        return 0;
+    }
     const scrollNext = () => {
-        flatListRef.current?.scrollToIndex({ animated: true, index: positionSlider + 1 > ArrayItems.length - 1 ? 0 : positionSlider + 1 });
+        flatListRef.current?.scrollToIndex(
+            {
+                animated: true,
+                index: positionSlider + 1 > ArrayItems.length - 1
+                    ? canGoLogin()
+                    : positionSlider + 1
+            });
     };
     return (
         <Box>

b14398ab08450148e798c9213165410601fbca7d
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-22 19:41:20 -0300

    feat: created basic pages

---
+++ src/screen/auth/loginScreen/loginScreen.tsx
@@ -0,0 +1,10 @@
+import { Screen } from '@components';
+import React from 'react';
+
+export function LoginScreen() {
+    return (
+        <Screen canGoBack>
+            <></>
+        </Screen>
+    );
+}
---
+++ src/screen/auth/signUpScreen/signUpScreen.tsx
@@ -0,0 +1,10 @@
+import { Screen } from '@components';
+import React from 'react';
+
+export function SignUpScreen() {
+    return (
+        <Screen canGoBack>
+            <></>
+        </Screen>
+    );
+}

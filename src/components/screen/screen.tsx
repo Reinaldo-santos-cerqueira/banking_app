cb0d8ab4a21ba6baa1c3c297ea5166071606b849
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-22 19:42:09 -0300

    feat: added function goBack

--- src/components/screen/screen.tsx
+++ src/components/screen/screen.tsx
@@ -3,6 +3,7 @@ import { Box, Icon } from '@components';
 import { KeyboardAvoidingView, Platform, Pressable, SafeAreaView, StatusBar } from 'react-native';
 import { ScrollViewContainer, ViewContainer } from './Container';
 import { useAppSafeArea, useAppTheme } from '@hooks';
+import { useNavigation } from '@react-navigation/native';
 
 interface ScreenProps {
     canGoBack?: boolean;
@@ -10,6 +11,8 @@ interface ScreenProps {
     scrollable?: boolean;
 }
 export function Screen({ children, canGoBack, scrollable }: ScreenProps) {
+    const { goBack } = useNavigation();
+
     const { top, bottom } = useAppSafeArea();
     const { colors } = useAppTheme();
     const Container = scrollable ? ScrollViewContainer : ViewContainer;
@@ -30,7 +33,7 @@ export function Screen({ children, canGoBack, scrollable }: ScreenProps) {
                     >
                         {
                             canGoBack &&
-                            <Pressable onPress={() => console.log('Teste')}>
+                            <Pressable onPress={goBack}>
                                 <Box mb={'s24'} width={42} height={42} borderRadius={'s42'} backgroundColor='backgroundContrast' alignItems='center' justifyContent='center'>
                                     <Icon name='backArrow' />
                                 </Box>

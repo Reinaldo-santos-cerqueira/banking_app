d7935396ae1928efb356a3283ebf0402029f10f2
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-22 19:50:35 -0300

    feat: export element folder screen and routes

---
+++ src/routes/index.ts
@@ -0,0 +1,3 @@
+export * from './routes';
+export * from './navigationTypes';
+export * from './authStack.routes';
--- src/screen/index.ts
+++ src/screen/index.ts
@@ -1,1 +1,4 @@
 export * from './auth/onBoardingScreen/onBoardingScreen';
+export * from './auth/loginScreen/loginScreen';
+export * from './auth/signUpScreen/signUpScreen';
+export * from './app/home/homeScreen';

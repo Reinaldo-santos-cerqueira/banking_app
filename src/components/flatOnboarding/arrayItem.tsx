ce32cefd49811b86d369f483efc4848ffed4fc00
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-22 18:09:49 -0300

    feat: creted array with configuration of caurosel

---
+++ src/components/flatOnboarding/arrayItem.tsx
@@ -0,0 +1,23 @@
+export interface IArrayItems {
+    svg: number;
+    title: string;
+    subTitle: string;
+}
+
+export const ArrayItems: IArrayItems[] = [
+    {
+        svg: 1,
+        title: 'Fastest Payment in\n the world',
+        subTitle: 'Integrate multiple payment methoods\n to help  you up the process quickly'
+    },
+    {
+        svg: 2,
+        title: 'The most Secoure\n Platfrom for Customer',
+        subTitle: 'Built-in Fingerprint, face recognition\n  and more, keeping you completely safe'
+    },
+    {
+        svg: 3,
+        title: 'Paying for Everything is\n Easy and Convenient',
+        subTitle: 'Built-in Fingerprint, face recognition\n and more, keeping you completely safe'
+    }
+];

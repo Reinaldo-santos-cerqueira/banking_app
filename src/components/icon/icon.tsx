cffd5fe517ad0a3d2a41249dfde272100476d1b6
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-22 13:25:15 -0300

    feat: created icon components vinculed with svg export assets

---
+++ src/components/icon/icon.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useAppTheme } from '@hooks';
+import { ThemeColors } from '@theme';
+import { Pressable } from 'react-native';
+import { BackArrow } from '@assets';
+
+export interface IconBase {
+    size?: number;
+    color?: string;
+    circleColor?: string
+}
+
+export interface IconProps {
+    name: IconName;
+    color?: ThemeColors;
+    size?: number;
+    onPress?: () => void;
+    circleColor?: ThemeColors
+}
+export function Icon({ name, color = 'backgroundContrast', size, onPress, circleColor = 'backgroundContrast' }: IconProps) {
+    const { colors } = useAppTheme();
+    const SVGIcon = iconRegistry[name];
+    if (onPress) {
+        return (
+            <Pressable
+                onPress={onPress}
+                hitSlop={10}
+            >
+                <SVGIcon color={colors[color]} size={size} circleColor={colors[circleColor]} />
+            </Pressable>
+        );
+    }
+    return <SVGIcon color={colors[color]} size={size} circleColor={colors[circleColor]} />;
+}
+
+const iconRegistry = {
+    backArrow: BackArrow,
+};
+
+type IconType = typeof iconRegistry;
+
+export type IconName = keyof IconType;

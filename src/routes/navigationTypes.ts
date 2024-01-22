f88fb59607baa54a121975f543f019eb82d9bbd5
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-22 19:49:27 -0300

    feat: created configuration screens props'

---
+++ src/routes/navigationTypes.ts
@@ -0,0 +1,12 @@
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { AppStackParams } from './app.routes';
+import { AuthStackParams } from './authStack.routes';
+
+declare global {
+    // eslint-disable-next-line @typescript-eslint/no-namespace
+    namespace ReactNavigation {
+        interface RootParamList extends AuthStackParams, AppStackParams { }
+    }
+}
+export type AppScreenProps<RouteName extends keyof AppStackParams> = NativeStackScreenProps<AppStackParams, RouteName>
+export type AuthScreenProps<RouteName extends keyof AuthStackParams> = NativeStackScreenProps<AuthStackParams, RouteName> 

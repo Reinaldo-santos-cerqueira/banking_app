f8993e0f39ee2f125c8781391291f23f2b7096ba
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-25 20:21:04 -0300

    feat: configure react hook form with zod and login schema

--- src/screen/auth/loginScreen/loginScreen.tsx
+++ src/screen/auth/loginScreen/loginScreen.tsx
@@ -1,18 +1,49 @@
-import { Box, Button, Screen, Text, TextInput } from '@components';
+import { Box, Button, Screen, Text, TextInputWithController } from '@components';
 import { AuthScreenProps } from '@routes';
-import React from 'react';
+import React, { useState } from 'react';
 import { Pressable } from 'react-native';
+import {
+    useForm
+} from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { LoginSchema, loginSchema } from './loginSchema';
 
 export function LoginScreen({ navigation }: AuthScreenProps<'LoginScreen'>) {
+    const { control, formState, handleSubmit } = useForm<LoginSchema>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+        },
+        mode: 'onChange'
+    });
+
+    const [loadingBtn, setLoadingBtn] = useState(false);
+
+    const goToSignUp = () => {
+        navigation.navigate('SignUpScreen');
+    };
+    const login = async () => {
+        setLoadingBtn(true);
+        await new Promise(resolve => setTimeout(() => {
+            resolve('');
+        }, 2000));
+        setLoadingBtn(false);
+
+    };
     return (
         <Screen canGoBack>
             <Text variant='titlePage' mb='s32'>Sign In</Text>
-            <TextInput icon='email' label='Email address' placeholder='Enter email' />
-            <TextInput icon='padLock' label='Password' placeholder='Enter password' isPassword />
-            <Button title='Sign In' mt={'s20'} mb='s32' />
+            <TextInputWithController control={control} name='email' icon='email' label='Email address' placeholder='Enter email' />
+            <TextInputWithController name='password' control={control} icon='padLock' label='Password' placeholder='Enter password' isPassword />
+            <Button
+                disabled={!formState.isValid}
+                title='Sign In'
+                onPress={handleSubmit(login)} loading={loadingBtn} mt={'s20'} mb='s32'
+            />
             <Box flexDirection='row' justifyContent='center'>
                 <Text variant='labelText'>{'I\'m a new user.'}</Text>
-                <Pressable onPress={() => navigation.navigate('SignUpScreen')}>
+                <Pressable onPress={goToSignUp}>
                     <Text color='bluePrimary' fontFamily='Poppins-Medium' variant='labelText'>Sign In</Text>
                 </Pressable>
             </Box>

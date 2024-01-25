1fb4a983ac16bd12064db9c79afec9ccfe6e2262
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-01-25 20:21:40 -0300

    feat: configure react hook form with zod and signup schema

--- src/screen/auth/signUpScreen/signUpScreen.tsx
+++ src/screen/auth/signUpScreen/signUpScreen.tsx
@@ -1,17 +1,64 @@
-import { Box, Button, Screen, Text, TextInput } from '@components';
+import { Box, Button, Screen, Text, TextInputWithController } from '@components';
 import { AuthScreenProps } from '@routes';
-import React from 'react';
+import React, { useState } from 'react';
 import { Pressable } from 'react-native';
+import { SignUpSchema, signupSchema } from './signUpSchema';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export function SignUpScreen({ navigation }: AuthScreenProps<'SignUpScreen'>) {
+    const [loadingBtn, setLoadingBtn] = useState(false);
+
+    const { control, formState, handleSubmit } = useForm<SignUpSchema>({
+        resolver: zodResolver(signupSchema),
+        defaultValues: {
+            email: '',
+            password: '',
+            fullName: '',
+            phone: '',
+        },
+        mode: 'onChange'
+    });
+    const signUp = async () => {
+        setLoadingBtn(true);
+        await new Promise(resolve => setTimeout(() => {
+            resolve('');
+        }, 2000));
+        setLoadingBtn(false);
+
+    };
     return (
-        <Screen canGoBack>
+        <Screen scrollable canGoBack>
             <Text variant='titlePage' mb='s32'>Sign Up</Text>
-            <TextInput icon='user' label='Full Name' placeholder='Enter full name' />
-            <TextInput icon='phone' label='Phone Number' placeholder='Enter phone number' />
-            <TextInput icon='email' label='Email address' placeholder='Enter email' />
-            <TextInput icon='padLock' label='Password' placeholder='Enter password' isPassword />
-            <Button title='Sign Up' mt={'s20'} mb='s32' />
+            <TextInputWithController name='fullName' control={control} icon='user' label='Full Name' placeholder='Enter full name' />
+            <TextInputWithController
+                name='phone'
+                control={control}
+                icon='phone'
+                keyboardType='number-pad'
+                label='Phone Number'
+                placeholder='Enter phone number'
+            />
+            <TextInputWithController
+                name='email'
+                control={control}
+                icon='email'
+                label='Email address'
+                placeholder='Enter email'
+            />
+            <TextInputWithController
+                name='password'
+                control={control}
+                icon='padLock'
+                label='Password'
+                placeholder='Enter password'
+                isPassword
+            />
+            <Button
+                disabled={!formState.isValid}
+                title='Sign Up'
+                onPress={handleSubmit(signUp)} loading={loadingBtn} mt={'s20'} mb='s32'
+            />
             <Box flexDirection='row' justifyContent='center'>
                 <Text variant='labelText'>{'Already have an account.'}</Text>
                 <Pressable onPress={() => navigation.navigate('LoginScreen')}>

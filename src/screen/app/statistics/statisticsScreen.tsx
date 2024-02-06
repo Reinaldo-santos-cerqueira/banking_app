ec6e50162f9686a168e475984e1c26560a402440
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-02-06 18:54:34 -0300

    feat: new screens added

---
+++ src/screen/app/statistics/statisticsScreen.tsx
@@ -0,0 +1,10 @@
+import { Screen } from '@components';
+import React from 'react';
+
+export function StatisticsScreen() {
+    return (
+        <Screen>
+            <></>
+        </Screen>
+    );
+}
--- src/screen/index.ts
+++ src/screen/index.ts
@@ -2,3 +2,6 @@ export * from './auth/onBoardingScreen/onBoardingScreen';
 export * from './auth/loginScreen/loginScreen';
 export * from './auth/signUpScreen/signUpScreen';
 export * from './app/home/homeScreen';
+export * from './app/settings/settingsScreen';
+export * from './app/statistics/statisticsScreen';
+export * from './app/myCard/myCardScreen';

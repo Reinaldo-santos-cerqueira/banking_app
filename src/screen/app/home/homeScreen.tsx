d6890da51c88e8c782b84e857e7867027588754c
Reinaldo-santos-cerquira <kingnaldoreinaldo@gmail.com>
2024-02-07 17:48:22 -0300

    feat: added area header

--- src/screen/app/home/homeScreen.tsx
+++ src/screen/app/home/homeScreen.tsx
@@ -1,10 +1,28 @@
-import { Screen } from '@components';
+import { CardBg, ProfileImagem } from '@assets';
+import { Box, Icon, Profile, Screen } from '@components';
 import React from 'react';
+import { Image } from 'react-native';
 
 export function HomeScreen() {
     return (
         <Screen>
-            <></>
+            <Box
+                flexDirection='row'
+                mb='s20'
+                mt='s20'
+            >
+                <Profile name='Reinaldo Santos' title='Welcome back' photo={ProfileImagem} />
+                <Icon name='search' color='grayWhite' colorArea='backgroundContrast' />
+            </Box>
+            <Box
+                width={'100%'}
+                height={200}
+                backgroundColor={'bgCardArea'}
+                borderRadius='s25'
+                position='relative'
+            >
+                <Image source={CardBg} height={200} style={{ position: 'absolute', top: 0, left: 0 }} />
+            </Box>
         </Screen>
     );
 }

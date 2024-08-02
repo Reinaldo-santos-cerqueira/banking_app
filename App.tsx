21ab6b65db64a731d59831fc4ac0a8e92511bde9
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-02 20:17:00 -0300

    feat: added Provider to project root

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
+import { Provider } from 'react-redux';
 import { Router } from '@routes';
 import { ThemeProvider } from '@shopify/restyle';
 import { theme } from '@theme';
 import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SplashScreen from 'react-native-splash-screen';
+import {store} from '@redux';
 
 function App(): React.ReactNode {
     useEffect(() => {
         SplashScreen.hide();
     }, []);
     return (
-        <SafeAreaProvider>
-            <ThemeProvider theme={theme}>
-                <Router />
-            </ThemeProvider>
-        </SafeAreaProvider>
+        <Provider store={store}>
+            <SafeAreaProvider>
+                <ThemeProvider theme={theme}>
+                    <Router />
+                </ThemeProvider>
+            </SafeAreaProvider>
+        </Provider>
     );
 }
 

f3ffb63e491248c211638967ed19b2c3e10ab4a9
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-02 20:07:38 -0300

    chore: change linebreak-style  'windows' to 'unix'

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,10 +35,7 @@ module.exports = {
             'error',
             4
         ],
-        'linebreak-style': [
-            'error',
-            'windows'
-        ],
+        'linebreak-style': ['error', 'unix'],
         'quotes': [
             'error',
             'single'

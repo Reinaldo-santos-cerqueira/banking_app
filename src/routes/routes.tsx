bfff9345464ea259d40421c0837a930228b56733
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-02 20:13:36 -0300

    refactor: added useSelector to logged of loginSlice

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,9 +2,12 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { AuthStack } from './authStack.routes';
 import { AppStack } from './appStack.routes';
+import { useSelector } from 'react-redux';
+import { RootState } from '@redux';
 
 export function Router() {
-    const logged = true;
+    const logged = useSelector((state: RootState) => state.login.logged);
+
     return (
         <NavigationContainer>
             {

d5a92461152bd21dd18d6bd09609d1476a5030d4
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-06 20:32:30 -0300

    chore: added react native reanimated

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,7 @@ module.exports = {
                 '@domain': './src/domain',
                 '@redux':'./src/redux'
             }
-        }]
+        }],
+        'react-native-reanimated/plugin',
     ]
 };
